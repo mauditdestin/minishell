9f3700f8d0e65cbcb5614ea710c3b3a761c38c69
catlover4242 <akapaui@protonmail.com>
2025-06-28 23:37:44 +0200

    + | Did ft_echo, need to do -n options

--- builtins/echo.c
+++ builtins/echo.c
@@ -10,3 +10,28 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/minishell.h"
+
+void	ft_echo(char **argv)
+{
+	int i;
+	int print_newline;
+
+	i = 1;
+	print_newline = 1;
+
+	while (argv[i] && is_n_option(argv[i]))
+	{
+		print_newline = 0;
+		i++;
+	}
+	while (argv[i])
+	{
+		write(1, argv[i], ft_strlen(argv[i]));
+		if (argv[i + 1])
+			write(1, " ", 1);
+		i++;
+	}
+	if (print_newline)
+		write(1, "\n", 1);
+}
